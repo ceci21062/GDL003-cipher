eeebab05dca89994eba287bdd3cb506e9ff2b45f
Didre <ixchel@hotmail.es>
2019-06-02 18:42:52 -0500

    introduccion

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log"hola mundo "
+
+//pag de inicio
+document.getElementById("introduccion").innerHTML = "CLASIFICADO"
+document.getElementById("objetivo").innerHTML = "Esta app esta diseñada para codificar el nombre de cualquier archivo"
+
+function menu(){
+  
+}
