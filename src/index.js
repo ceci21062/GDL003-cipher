54df53454c3bf09d51dba233c3a42a1c432f0f1b
Didre <ixchel@hotmail.es>
2019-06-04 15:12:08 -0500

    menu

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
+document.getElementById('intro').style.display = 'block';
+document.getElementById('menu').style.display = 'none'
 
 //pag de inicio
 document.getElementById("introduccion").innerHTML = "CLASIFICADO"
 document.getElementById("objetivo").innerHTML = "Esta app esta diseñada para codificar el nombre de cualquier archivo"
+
+
+function menu()
+{
+  document.getElementById('menu').style.display = 'block';
+  document.getElementById('intro').style.display = 'none';
+}
