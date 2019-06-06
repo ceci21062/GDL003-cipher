42bfa7cfff408cc3222f45e2d4171d9f57abd111
Didre <ixchel@hotmail.es>
2019-06-06 15:07:14 -0500

    decodificar

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,5 @@
 window.cipher = {
-  encode: function (){
+  encode: function(){
     let txt = document.getElementById("codigo").value;
     let desplazar= document.getElementById("numeroBase").value;
     let codi = '';
@@ -9,11 +9,25 @@ window.cipher = {
       let suma2= suma1 + desplazar;
       let suma3= suma2 % 26;
       let suma4= suma3 + 65;
-      let txtCo=String.fromCharCode(suma4)+" ";
-      codi += txtCo
+      let txtCo=String.fromCharCode(suma4);
+      codi += txtCo;
     }
     return codi;
+  },
+  dencode: function(){
+    let codiAres=document.getElementById("clave").value;
+    let numSecret=document.getElementById("numeroSecreto").value;
+    let res= "";
+
+    for (var i = 0; i <=codiAres.length; i++){
+      let rest1 = codiAres.charCodeAt(i)+ 65;
+      let rest2 = rest1 - numSecret;
+      let rest3 = rest2 % 26;
+      let rest4 = rest3 + 65;
+      let clavCo = String.fromCharCode(rest4);
+      res += clavCo;
+      }
+    return res;
   }
-  
   // ...
-};
+}
--- src/index.js
+++ src/index.js
@@ -1,42 +1,53 @@
 
 document.getElementById('intro').style.display = 'block';
-document.getElementById('menu').style.display = 'none'
-document.getElementById("codificar").style.display='none'
+document.getElementById('menu').style.display = 'none';
+document.getElementById("codificar").style.display='none';
+document.getElementById("decodificar").style.display="none";
 
 //pag de inicio
-document.getElementById("introduccion").innerHTML = "CLASIFICADO"
-document.getElementById("objetivo").innerHTML = "Esta app esta diseñada para codificar el nombre de cualquier archivo"
+document.getElementById("introduccion").innerHTML = "CLASIFICADO";
+document.getElementById("objetivo").innerHTML = "Esta app esta diseñada para codificar el nombre de cualquier archivo";
 
 
-function menu()
-{
+function menu() {
   document.getElementById('menu').style.display = 'block';
   document.getElementById('intro').style.display = 'none';
-  document.getElementById("codificar").style.display='none'
+  document.getElementById("codificar").style.display='none';
+  document.getElementById("decodificar").style.display="none";
 
-  document.getElementById("requisitos").innerHTML= "Para codificar es necesario tener en cuenta los sigueintes requisitos"
-  document.getElementById("reqUno").innerHTML="El nombre debe estar en mayusculas"
-  document.getElementById("reqDos").innerHTML="No incluir simbolos"
-  document.getElementById("reqTre").innerHTML="No incluir la Ñ"
-  document.getElementById("reqCua").innerHTML="sin espacios"
+  document.getElementById("requisitos").innerHTML= "Para codificar es necesario tener en cuenta los sigueintes requisitos";
+  document.getElementById("reqUno").innerHTML="El nombre debe estar en mayusculas";
+  document.getElementById("reqDos").innerHTML="No incluir simbolos";
+  document.getElementById("reqTre").innerHTML="No incluir la Ñ";
+  document.getElementById("reqCua").innerHTML="sin espacios";
 
-  document.getElementById("requisitos1").innerHTML= "Para codificar es necesario tener en cuenta los sigueintes requisitos"
-  document.getElementById("reqCin").innerHTML="El nombre debe estar en mayusculas"
-  document.getElementById("reqSei").innerHTML="No incluir simbolos"
-  document.getElementById("reqSie").innerHTML="No incluir la Ñ"
-  document.getElementById("reqOch").innerHTML="sin espacios"
-  document.getElementById("reqNue").innerHTML="Tener a la mano el Numero Base"
-
+  document.getElementById("requisitos1").innerHTML= "Para codificar es necesario tener en cuenta los sigueintes requisitos";
+  document.getElementById("reqCin").innerHTML="El nombre debe estar en mayusculas";
+  document.getElementById("reqSei").innerHTML="No incluir simbolos";
+  document.getElementById("reqSie").innerHTML="No incluir la Ñ";
+  document.getElementById("reqOch").innerHTML="sin espacios";
+  document.getElementById("reqNue").innerHTML="Tener a la mano el Numero Base";
 }
 
-function codificar()
-{
-  document.getElementById("codificar").style.display='block'
-  document.getElementById('menu').style.display ='none'
-  document.getElementById('intro').style.display ='none'
+function codificar(){
+  document.getElementById("codificar").style.display='block';
+  document.getElementById('menu').style.display ='none';
+  document.getElementById('intro').style.display ='none';
+  document.getElementById("decodificar").style.display="none";
 
   document.getElementById("enviarCodigo").addEventListener("click",function(){
-    document.getElementById("texto").innerHTML= window.cipher.encode();
-});
+    document.getElementById("texto").innerHTML = window.cipher.encode();
+  });
+}
 
+
+function decodificar(){
+  document.getElementById("decodificar").style.display='block';
+  document.getElementById("codificar").style.display='none';
+  document.getElementById('menu').style.display ='none';
+  document.getElementById('intro').style.display ='none';
+
+  document.getElementById("enviarClave").addEventListener("click",function(){
+    document.getElementById("codiDesi").innerHTML = window.cipher.dencode();
+  });
 }
