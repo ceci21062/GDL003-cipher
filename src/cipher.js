f6b6f2466e2483724b950c611b7a8acceccd4e23
Didre <ixchel@hotmail.es>
2019-06-06 16:22:18 -0500

    convirtiendo a numero

--- src/cipher.js
+++ src/cipher.js
@@ -5,23 +5,37 @@ window.cipher = {
     let codi = '';
 
     for(i=0; i<=txt.length;i++){
-      let suma1=txt.charCodeAt(i)-65;
-      let suma2= suma1 + desplazar;
-      let suma3= suma2 % 26;
-      let suma4= suma3 + 65;
-      let txtCo=String.fromCharCode(suma4);
+      let conLet = txt.charCodeAt(i);
+      let eje = parseInt(conLet)
+      console.log(eje)
+      let sumaPri = eje - 65;
+      console.log(sumaPri)
+      let desNum= parseInt(desplazar)
+      let sumaDes = sumaPri + desNum;
+      console.log(sumaDes)
+      let sumaRes = sumaDes % 26;
+      console.log(sumaRes)
+      let sumaTot = sumaRes + 65;
+      console.log(sumaTot)
+      let txtCo = String.fromCharCode(sumaTot);
+      console.log(String.fromCharCode(sumaTot))
       codi += txtCo;
+
     }
     return codi;
   },
+
   dencode: function(){
     let codiAres=document.getElementById("clave").value;
     let numSecret=document.getElementById("numeroSecreto").value;
     let res= "";
 
     for (var i = 0; i <=codiAres.length; i++){
-      let rest1 = codiAres.charCodeAt(i)+ 65;
-      let rest2 = rest1 - numSecret;
+      let codiAss = codiAres.charCodeAt(i)
+      let eje2= parseInt(codiAss)
+      let restPri = eje2 + 65;
+      let conNumSec=parseInt(numSecret)
+      let rest2 = restPri - conNumSec;
       let rest3 = rest2 % 26;
       let rest4 = rest3 + 65;
       let clavCo = String.fromCharCode(rest4);
