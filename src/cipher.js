1d46a75901932b671589335f778328b06f2d3b62
Didre <ixchel@hotmail.es>
2019-06-05 21:56:27 -0500

    codificar

--- src/cipher.js
+++ src/cipher.js
@@ -1,22 +1,19 @@
 window.cipher = {
+  encode: function (){
+    let txt = document.getElementById("codigo").value;
+    let desplazar= document.getElementById("numeroBase").value;
+    let codi = '';
 
-  encode: function ( ){
-  let txt=document.getElementById("codigo").value
-  let numBase=document.getElementById("numeroBase").value
-  //document.getElementById("num").innerHTML= txt.length
-  //for(i=0; i<=txt.length;i++){
-  //let cadena=txt[i];
-  //document.getElementById("unoPorUno").innerHTML=cadena+ " "
-  //document.getElementById("textMasAsc").innerHTML= txt.charCodeAt(i)+ " "
-  //let suma1=txt.charCodeAt(i)-ASC
-  //let suma2= suma1 + numBase
-  //let suma3= suma2 % ABC
-  //let suma4= suma3 + ASC
-  //document.getElementById("nuevotxtMasAsc").innerHTML= suma3 + " "
-  //document.getElementById("nuevotxt").innerHTML=String.fromCharCode(suma4)+" ":
-  return txt
+    for(i=0; i<=txt.length;i++){
+      let suma1=txt.charCodeAt(i)-65;
+      let suma2= suma1 + desplazar;
+      let suma3= suma2 % 26;
+      let suma4= suma3 + 65;
+      let txtCo=String.fromCharCode(suma4)+" ";
+      codi += txtCo
+    }
+    return codi;
   }
-
-
+  
   // ...
 };
